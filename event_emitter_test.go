51b297a08c6568f2b888b39c176e8d34ea5eb6d8
Timur Gilauri <timur.gilauri@hotmail.com>
2023-08-13 13:33:35 +0600

    chore: add test case for different event data types

--- event_emitter_test.go
+++ event_emitter_test.go
@@ -18,7 +18,7 @@ func TestEmitterShouldAddListeners(t *testing.T) {
 
 	emitter.Emit("event", "Event emitted")
 
-	assert.Equal(t, callCounter, 1, "Callback should be called 1 time")
+	assert.Equal(t, 1, callCounter, "Callback should be called 1 time")
 }
 
 func TestEmitterShouldRemoveListeners(t *testing.T) {
@@ -26,6 +26,7 @@ func TestEmitterShouldRemoveListeners(t *testing.T) {
 	callCounter := 0
 	cb := func(event string) {
 		fmt.Println(event)
+		callCounter += 1
 	}
 
 	emitter.On("event", cb)
@@ -33,5 +34,48 @@ func TestEmitterShouldRemoveListeners(t *testing.T) {
 
 	emitter.Emit("event", "Event emitted")
 
-	assert.Equal(t, callCounter, 0, "Callback should be called 0 times")
+	assert.Equal(t, 0, callCounter, "Callback should be called 0 times")
+}
+
+func TestEmitterShouldRunAllListeners(t *testing.T) {
+	emitter := NewEventEmitter[string](10)
+	callCounter := 0
+	cb := func(event string) {
+		fmt.Println(event)
+		callCounter += 1
+	}
+
+	cb2 := func(event string) {
+		fmt.Println(event)
+		callCounter += 1
+	}
+
+	emitter.On("event", cb)
+	emitter.On("event", cb2)
+
+	emitter.Emit("event", "Event emitted")
+
+	assert.Equal(t, 2, callCounter, "Callback should be called 2 times")
+}
+
+func TestEmitterShouldRunListenersWithDifferentEventTypes(t *testing.T) {
+	emitter := NewEventEmitter[any](10)
+	callCounter := 0
+	cb := func(event any) {
+		fmt.Println(event)
+		callCounter += 1
+	}
+
+	cb2 := func(event any) {
+		fmt.Println(event)
+		callCounter += 1
+	}
+
+	emitter.On("event", cb)
+	emitter.On("someOther", cb2)
+
+	emitter.Emit("event", "Event emitted")
+	emitter.Emit("someOther", 1)
+
+	assert.Equal(t, 2, callCounter, "Callback should be called 2 times")
 }
