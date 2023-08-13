da710888d6c0bc012be48fd3bbd07b4c88ea4136
Timur Gilauri <timur.gilauri@hotmail.com>
2023-08-13 12:56:29 +0600

    feat: add assertions to test

--- event_emitter_test.go
+++ event_emitter_test.go
@@ -1,16 +1,21 @@
 package event_emitter
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEmitterShouldAddListeners(t *testing.T) {
 	emitter := NewEventEmitter[string](10)
+	callCounter := 0
+	cb := func(event string) {
+		callCounter += 1
+	}
 
-	emitter.On("event", func(event string) {
-		fmt.Println(event)
-	})
+	emitter.On("event", cb)
 
 	emitter.Emit("event", "Event emitted")
+
+	assert.Equal(t, callCounter, 1, "Callback should be called 1 time")
 }
